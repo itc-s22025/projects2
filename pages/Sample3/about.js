77351034ec0e9064310a041ea3f7ae54e039c360
s22025 <s22025@std.it-college.ac.jp>
2023-01-18 07:44:18 +0000

    Project3 finished

--- pages/Sample3/about.js
+++ pages/Sample3/about.js
@@ -5,11 +5,17 @@ const Home = () =>{
 	return (<>	
 			<main>
 				<div className={style.body}>
+						<p className={style.trp}>a</p>
+						<div className={style.tr2}></div>
 					<div className={style.box}>
 						<h1 className={style.h1}>COMING SOON</h1>
 						<p className={style.p}>NEW WEBSITE IS AVAIRABLE <br />
 						ON 20XX.01.01</p>
+							<div className={style.tr}>
+								<p className={style.trp}>a</p>
+							</div>
 					</div>
+					<p className={style.bg}>a</p>
 				</div>
 			</main>
 
